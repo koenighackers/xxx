cd2e8d41816cdfffca930ffb9731a3e34214f214
Artem Ostapenko <aptemka39@gmail.com>
2018-02-18 14:13:50 +0200

    Redirect to shop page after login

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -89,7 +89,7 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+            return $this->redirect('s/'.Yii::$app->user->identity->username);
         } else {
             return $this->render('login', [
                 'model' => $model,
